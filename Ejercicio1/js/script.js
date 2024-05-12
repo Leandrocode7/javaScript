5b83b7f138ae7aef180d881ca7aef3ce460e815a
Leandrocode7 <leandronicolasgonzalez14@gmail.com>
2024-05-12 20:05:32 -0300

    updated

---
+++ Ejercicio1/js/script.js
@@ -0,0 +1,11 @@
+let edad = 8;
+
+
+if (edad <18 && edad>0){
+    console.log("Es menor de edad");
+
+}else if (edad >= 18){
+    console.log("Es mayor de edad");
+}else {
+    console.log("El valor ingresado es incorrecto")
+}
