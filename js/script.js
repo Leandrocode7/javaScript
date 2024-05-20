460503341d3f1ea8b0305a3636d86551a9dbaa91
Leandrocode7 <leandronicolasgonzalez14@gmail.com>
2024-05-20 19:53:21 -0300

    updated

--- js/script.js
+++ js/script.js
@@ -1,39 +1,121 @@
 
 let dineroUsuario = 0;
+let componente;
 let compra;
-let placaVideo= 800000;
-let peocesador= 450000;
-let mother = 400000;
-const IVA = 0.21;
+let accion;
+let acumulador = 0;
+let contador= 0;
+const PLACA = 800000;
+const PROCESADOR = 450000;
+const MOTHER = 400000;
+let inicio=1
 
+function cuenta (a,b){
+    calculo = a-b;
+}
+
+while(inicio==1){
+alert("A continuaciones seleccione la accion que desea realizar");
+accion = prompt("1. Cargar saldo | 2.Realizar compra | ESC.Salir del programa" )
+
+
+if (accion==1){
+ acumulador = contador;
  dineroUsuario = parseFloat (prompt ("Cuanto dinero quiere ingresar a su cuenta: " ));
+ 
 while (dineroUsuario<0){
 alert("El monto ingresado es incorrecto, vuelva a intentarlo");
-dineroUsuario = prompt ("Cuanto dinero quiere ingresar a su cuenta: " );
+dineroUsuario = parseFloat (prompt ("Cuanto dinero quiere ingresar a su cuenta: " ));
 }
-console.log(dineroUsuario);
-alert ("Saldo actual: $"+ dineroUsuario);
+contador = acumulador + dineroUsuario
+alert ("Saldo actual: $"+ parseFloat(contador));
 
 
+}
 
-compra= parseInt (prompt("Ingrese que la opcion del componente que desea comprar: 1-Placa de video, 2-Micro Procesador, 3-Motherboard"));
+else if (accion==2){
+componente= parseInt (prompt("Ingrese que la opcion del componente que desea comprar: 1-Placa de video, 2-Micro Procesador, 3-Motherboard"));
 
-while ((compra!=1) &&(compra!=2) && (compra!=3)){
+while ((componente!=1) &&(componente!=2) && (componente!=3)){
    alert ("El valor ingresado es incorrecto");
-    compra= parseInt (prompt("Ingrese que la opcion del componente que desea comprar: 1-Placa de video, 2-Micro Procesador, 3-Motherboard"));
+   componente= parseInt (prompt("Ingrese que la opcion del componente que desea comprar: 1-Placa de video, 2-Micro Procesador, 3-Motherboard"));
 }
 
-switch (compra){
+
+switch (componente){
     case 1: 
         alert("A seleccionado placa de video");
+        compra= prompt ("El precio es de $800000. Si desea continuar con su compra presione 1, para salir presione ESC");
+        while(compra !=1 && compra!="ESC"){
+            alert("Valor opcion incorrecta, vuelva a intentarlo")
+            compra= prompt ("El precio es de $800000. Si desea continuar con su compra presione 1, para salir presione ESC");
+        }
+        if(compra==1){
+           cuenta (contador,PLACA);
+            
+        console.log(calculo);
+           
+            if(calculo >= 0){
+                alert ("La compra se ha realizado con exito. Su saldo actual es de: " + calculo);
+            } else 
+            alert("No dispone con el dinero suficiente para realizar dicha compra");
+                
+        }else if (compra=="ESC")
+        alert ("Compra cancelada");
+
 
     break;
     case 2:
          alert("A seleccionado Micro procesador");
+         compra= prompt ("El precio es de $450000. Si desea continuar con su compra presione 1, para salir presione ESC");
+         while(compra !=1 && compra!="ESC"){
+            alert("Valor opcion incorrecta, vuelva a intentarlo")
+            compra= prompt ("El precio es de $450000. Si desea continuar con su compra presione 1, para salir presione ESC");
+        }
+         
+         if(compra==1){
+            cuenta (contador,PROCESADOR);
+             
+         console.log(calculo);
+            
+             if(calculo >= 0){
+                 alert ("La compra se ha realizado con exito. Su saldo actual es de: " + calculo);
+             } else 
+             alert("No dispone con el dinero suficiente para realizar dicha compra");
+                 
+         }else if (compra=="ESC")
+            alert ("Compra cancelada");
+    
+ 
     break;
      case 3:
          alert("A seleccionado Motherboard");
+         compra= prompt ("El precio es de $400000. Si desea continuar con su compra presione 1, para salir presione ESC");
+         while(compra !=1 && compra!="ESC"){
+            alert("Valor opcion incorrecta, vuelva a intentarlo")
+            compra= prompt ("El precio es de $400000. Si desea continuar con su compra presione 1, para salir presione ESC");
+        }
+        
+         if(compra==1){
+            cuenta (contador,MOTHER);
+             
+         console.log(calculo);
+            
+             if(calculo >= 0){
+                 alert ("La compra se ha realizado con exito. Su saldo actual es de: " + calculo);
+             } else 
+             alert("No dispone con el dinero suficiente para realizar dicha compra");
+                 
+         }else if (compra=="ESC")
+            alert ("Compra cancelada");
+    
+ 
     break;
  
 
+}
+}else if (accion=="ESC"){
+    inicio=0
+    alert("Fin del programa");
+}
 }
